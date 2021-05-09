8d2b07c0c8900a4c45d5de2422c29de92bd58713
saleem <saleemmalikraja@gmil.com>
2021-05-09 15:33:23 +0530

    updated to angular 8

--- src/app/app.module.imports.ts
+++ src/app/app.module.imports.ts
@@ -1,13 +1,11 @@
-import {
-  MatToolbarModule,
-  MatButtonModule,
-  MatIconModule,
-  MatListModule,
-  MatCardModule,
-  MatInputModule,
-  MatTableModule,
-  MatDividerModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 
 import {MatChipsModule} from '@angular/material/chips';
